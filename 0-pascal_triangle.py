de4a0b891e211411433e1bd4e868479df020f8d7
Enesi Emmanuel Omeiza <emmymy2@gmail.com>
2024-06-30 18:16:30 +0000

    update

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -10,11 +10,15 @@ def pascal_triangle(n):
     if n <= 0:
         return triangle
     for i in range(n):
-        temp_list = []
+        row = []
         for j in range(i+1):
             if j == 0 or j == i:
-                temp_list.append(1)
+                row.append(1)
             else:
-                temp_list.append(triangle[i-1][j-1] + triangle[i-1][j])
-        triangle.append(temp_list)
+                row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        triangle.append(row)
     return triangle
+
+if __name__ == "__main__":
+    print(pascal_triangle(5))
+
